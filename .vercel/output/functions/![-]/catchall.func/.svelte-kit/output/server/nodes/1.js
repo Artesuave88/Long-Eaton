4cec3bc74855219723d913b624dae0c6480bc899
Michael Stevenson <michaeldstevenson1@outlook.com>
2026-03-27 12:03:18 +0000

    fix: update build script to use svelte-kit and add postcss configuration file

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DWwNFSwq.js","_app/immutable/chunks/Dcf3bkLG.js","_app/immutable/chunks/Cmn9UCS_.js","_app/immutable/chunks/B7ol3XSe.js","_app/immutable/chunks/CV8NvbTj.js","_app/immutable/chunks/Cqpwclfv.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/D-JH9fzP.js"];
+export const imports = ["_app/immutable/nodes/1.bVR_RTaL.js","_app/immutable/chunks/Dcf3bkLG.js","_app/immutable/chunks/Cmn9UCS_.js","_app/immutable/chunks/B7ol3XSe.js","_app/immutable/chunks/CV8NvbTj.js","_app/immutable/chunks/BFozC_JH.js","_app/immutable/chunks/BUApaBEI.js","_app/immutable/chunks/D-JH9fzP.js"];
 export const stylesheets = [];
 export const fonts = [];
